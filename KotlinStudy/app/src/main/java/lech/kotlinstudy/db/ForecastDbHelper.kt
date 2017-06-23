8b235312b842dd5f595acc8d7b373b448fbefc57
lch <chen.le@qliyun.com>
2017-06-23 14:27:43 +0800

    修改了部分错误信息

--- KotlinStudy/app/src/main/java/lech/kotlinstudy/db/ForecastDbHelper.kt
+++ KotlinStudy/app/src/main/java/lech/kotlinstudy/db/ForecastDbHelper.kt
@@ -3,16 +3,13 @@ package lech.kotlinstudy.db
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import lech.kotlinstudy.App
-import org.w3c.dom.Text
-import org.xmlpull.v1.XmlPullParser.TEXT
-import java.sql.Types.INTEGER
-
+import org.jetbrains.anko.db.*
 /**
  * Created by Android_61 on 2017/6/14.
  * Description
  * Others
  */
-class ForecastDbHelper(ctx: Context = App.instance) : ManagerSQLiteOpenHelper(App.instance, ForecastDbHelper.DB_NAME, null, ForecastDbHelper.DB_VERSION) {
+class ForecastDbHelper(ctx: Context = App.instance) : ManagedSQLiteOpenHelper(App.instance, ForecastDbHelper.DB_NAME, null, ForecastDbHelper.DB_VERSION) {
 
     companion object {
         val DB_NAME = "forecast.db"
@@ -20,7 +17,7 @@ class ForecastDbHelper(ctx: Context = App.instance) : ManagerSQLiteOpenHelper(Ap
         val instance: ForecastDbHelper by lazy { ForecastDbHelper() }
     }
 
-    override fun onCreate(db: SQLiteDatabase?) {
+    override fun onCreate(db: SQLiteDatabase) {
         db.createTable(CityForecastTable.NAME, true,
                 CityForecastTable.ID to INTEGER + PRIMARY_KEY,
                 CityForecastTable.CITY to TEXT,
@@ -38,7 +35,7 @@ class ForecastDbHelper(ctx: Context = App.instance) : ManagerSQLiteOpenHelper(Ap
                 DayForecastTable.CITY_ID to INTEGER)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         db.dropTable(CityForecastTable.NAME, true)
         db.dropTable(DayForecastTable.NAME, true)
         onCreate(db)
